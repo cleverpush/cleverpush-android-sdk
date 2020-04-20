2916e2c9a5eb9da539ff7f2bd7a44d26c7689304
Marius Gebhardt <marius.gebhardt@gmail.com>
2020-04-20 17:52:01 +0200

    Release 0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## Changelog
 
+== 0.6.0
+* Updated google services + gradle
+
 == 0.5.9 ==
 * Added `init` method only with channel ID parameter
 
--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -62,7 +62,7 @@ import java.util.Set;
 
 public class CleverPush implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, ActivityCompat.OnRequestPermissionsResultCallback {
 
-    public static final String SDK_VERSION = "0.5.9";
+    public static final String SDK_VERSION = "0.6.0";
 
     private static CleverPush instance;
 
