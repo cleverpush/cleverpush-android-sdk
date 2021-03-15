50fc8ca7a91684d5a23ffec242ae1f79fc3ecd4b
gbhrdt <marius.gebhardt@gmail.com>
2021-03-15 15:23:07 +0100

    Release 1.8.10

--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -81,7 +81,7 @@ import java.util.TimerTask;
 
 public class CleverPush implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, ActivityCompat.OnRequestPermissionsResultCallback {
 
-    public static final String SDK_VERSION = "1.8.9";
+    public static final String SDK_VERSION = "1.8.10";
 
     private static CleverPush instance;
 
