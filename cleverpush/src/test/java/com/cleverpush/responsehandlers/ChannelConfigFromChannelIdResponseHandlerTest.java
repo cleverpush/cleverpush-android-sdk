3b47f860cd969859c4e9a035454df03f8f48071b
HadikLakum <lakum@peerbits.com>
2021-08-05 16:50:26 +0530

    CP-1842  Updated branch from master

---
+++ cleverpush/src/test/java/com/cleverpush/responsehandlers/ChannelConfigFromChannelIdResponseHandlerTest.java
@@ -0,0 +1,23 @@
+package com.cleverpush.responsehandlers;
+
+import android.content.Context;
+
+import com.cleverpush.CleverPush;
+import com.cleverpush.CleverPushHttpClient;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+class ChannelConfigFromChannelIdResponseHandlerTest {
+
+
+
+}
