a4121c9ff16717646a2c63501f965411fddade59
HadikLakum <hardiklakum2012@gmail.com>
2021-08-05 16:09:05 +0530

    CP-2016  Resolved topic dialog exception

---
+++ cleverpush/src/main/java/com/cleverpush/util/Utils.java
@@ -0,0 +1,14 @@
+  package com.cleverpush.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.cleverpush.CleverPushPreferences;
+
+
+public class Utils {
+
+
+
+}
