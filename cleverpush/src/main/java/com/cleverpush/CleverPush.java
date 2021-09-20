b37d086296d23e0c32ca7addb950d18fa1ca00b2
gbhrdt <marius.gebhardt@gmail.com>
2021-09-20 17:51:51 +0200

    Release 1.16.2

--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -88,7 +88,7 @@ import java.util.TimerTask;
 
 public class CleverPush implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    public static final String SDK_VERSION = "1.16.1";
+    public static final String SDK_VERSION = "1.16.2";
 
     private static CleverPush instance;
 
