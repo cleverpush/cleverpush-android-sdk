b508411b0a9549a89376b9a32940c5a50b4ec54f
jprajapati1996 <104719696+jprajapati1996@users.noreply.github.com>
2022-10-05 23:11:17 +0530

    feature/CP-4535-android-support-device-ids
    CP-4535 - PR changes but still pending

--- cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
+++ cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
@@ -6,27 +6,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import java.util.UUID;
-
 public class DeviceIdBroadcastReceiver extends BroadcastReceiver {
-    String deviceId;
-
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences sharedPreferences = getSharedPreferences(context);
-        if (intent.getAction().equals(Constants.DEVICE_ID_INTENT)) {
-            if (intent.hasExtra(Constants.DEVICE_ID)) {
-                sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, intent.getStringExtra(Constants.DEVICE_ID)).apply();
-            }
-            deviceId = sharedPreferences.getString(CleverPushPreferences.DEVICE_ID, null);
-        } else {
-            sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, UUID.randomUUID().toString()).apply();
-            deviceId = UUID.randomUUID().toString();
+        if (intent.getAction().equals(Constants.DEVICE_ID_INTENT) && intent.hasExtra(Constants.DEVICE_ID)) {
+            sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, intent.getStringExtra(Constants.DEVICE_ID)).apply();
         }
     }
 
     public SharedPreferences getSharedPreferences(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
-
 }
