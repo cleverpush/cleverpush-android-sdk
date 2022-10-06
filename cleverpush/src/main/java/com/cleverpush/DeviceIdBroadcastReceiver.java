30768dad217fb023e888c8bf982b4321278a1103
jprajapati1996 <j.prajapati@cleverpush.com>
2022-10-06 20:22:32 +0530

    feature/CP-4535-android-support-device-ids
    CP-4535 PR changes

--- cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
+++ cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
@@ -7,19 +7,14 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class DeviceIdBroadcastReceiver extends BroadcastReceiver {
+    String deviceId;
     @Override
     public void onReceive(Context context, Intent intent) {
-        getActionName(Constants.APPLICATION_GROUP_NAME);
         SharedPreferences sharedPreferences = getSharedPreferences(context);
-        if (intent.getAction().equals(getActionName(Constants.APPLICATION_GROUP_NAME)) && intent.hasExtra(Constants.DEVICE_ID)) {
-            sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, intent.getStringExtra(Constants.DEVICE_ID)).apply();
+        if (intent.getAction().equals(Constants.DEVICE_ID_ACTION_KEY) && intent.hasExtra(Constants.DEVICE_ID)) {
+            deviceId = intent.getStringExtra(Constants.DEVICE_ID);
+            sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, deviceId).apply();
         }
-    }
-
-    private String getActionName(String applicationGroupName) {
-        String[] parts = applicationGroupName.split("\\.");
-        String splitedString = parts[0].concat(".").concat(parts[1]).concat(".").concat(Constants.DEVICE_ID_INTENT);
-        return splitedString;
     }
 
     public SharedPreferences getSharedPreferences(Context context) {
