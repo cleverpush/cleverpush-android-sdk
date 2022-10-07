ef64076f2c507585cdfd39d48772ebe203c2dcbe
jprajapati1996 <j.prajapati@cleverpush.com>
2022-10-07 10:45:52 +0530

    feature/CP-4535-android-support-device-ids
    CP-4535 - PR changes (define the variable inside of the function)

--- cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
+++ cleverpush/src/main/java/com/cleverpush/DeviceIdBroadcastReceiver.java
@@ -7,12 +7,11 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class DeviceIdBroadcastReceiver extends BroadcastReceiver {
-    String deviceId;
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences sharedPreferences = getSharedPreferences(context);
         if (intent.getAction().equals(Constants.DEVICE_ID_ACTION_KEY) && intent.hasExtra(Constants.DEVICE_ID)) {
-            deviceId = intent.getStringExtra(Constants.DEVICE_ID);
+            String deviceId = intent.getStringExtra(Constants.DEVICE_ID);
             sharedPreferences.edit().putString(CleverPushPreferences.DEVICE_ID, deviceId).apply();
         }
     }
