b6fb895ab2074aab486f0eed332b55992fc159b8
kevinrodriguesnobre <k.rodrigues@cleverpush.com>
2023-04-14 16:15:18 +0200

    Release 1.30.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.30.2
+* Fixed HTML AppBanner background color transparent by default
+
 ## 1.30.1
 * Added more customization options and `setWidgetId` function for `StoryView`
 
--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -122,7 +122,7 @@ import java.util.TimerTask;
 
 public class CleverPush {
 
-  public static final String SDK_VERSION = "1.30.1";
+  public static final String SDK_VERSION = "1.30.2";
 
   private static CleverPush instance;
   private static boolean isSubscribeForTopicsDialog = false;
