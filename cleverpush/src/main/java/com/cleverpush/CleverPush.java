00f2de4bf9a541755df1809539c2fba1a6b8fee9
kevinrodriguesnobre <k.rodrigues@cleverpush.com>
2023-05-10 17:23:08 +0200

    Release 1.30.8

--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -121,7 +121,7 @@ import java.util.TimerTask;
 
 public class CleverPush {
 
-  public static final String SDK_VERSION = "1.30.7";
+  public static final String SDK_VERSION = "1.30.8";
 
   private static CleverPush instance;
   private static boolean isSubscribeForTopicsDialog = false;
