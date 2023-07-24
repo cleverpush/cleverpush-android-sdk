72781f040b632ec82968799986f5f6cbb2cffa3c
Unnati Patadia <129381063+unnaticleverpush@users.noreply.github.com>
2023-07-24 14:22:42 +0530

    hotfix/CP-6312-flutter-chat-still-behaving-weirdly-when-re-subscribing-with-different-channel-id
    if the subscription id changed or unsubscribed then clear webview

--- cleverpush/src/main/java/com/cleverpush/manager/SubscriptionManagerBase.java
+++ cleverpush/src/main/java/com/cleverpush/manager/SubscriptionManagerBase.java
@@ -129,6 +129,12 @@ abstract class SubscriptionManagerBase implements SubscriptionManager {
           JSONObject responseJson = new JSONObject(response);
           if (responseJson.has("id")) {
             String newSubscriptionId = responseJson.getString("id");
+            String oldSubscriptionId = sharedPreferences.getString(CleverPushPreferences.SUBSCRIPTION_ID, null);
+            if (newSubscriptionId.equalsIgnoreCase(oldSubscriptionId)) {
+              CleverPush.getInstance(CleverPush.context).setSubscriptionChanged(false);
+            } else {
+              CleverPush.getInstance(CleverPush.context).setSubscriptionChanged(true);
+            }
             sharedPreferences.edit().putString(CleverPushPreferences.SUBSCRIPTION_ID, newSubscriptionId).apply();
             sharedPreferences.edit()
                 .putInt(CleverPushPreferences.SUBSCRIPTION_LAST_SYNC, (int) (System.currentTimeMillis() / 1000L))
