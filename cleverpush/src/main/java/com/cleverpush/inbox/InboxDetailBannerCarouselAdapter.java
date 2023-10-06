35802ee4cbe48d385f4c624d76a3ffd27425c908
Unnati Patadia <129381063+unnaticleverpush@users.noreply.github.com>
2023-10-06 10:55:10 +0530

    hotfix/CP-6788-android-app-banner-issues-v1
    The image scale not working on the banner

--- cleverpush/src/main/java/com/cleverpush/inbox/InboxDetailBannerCarouselAdapter.java
+++ cleverpush/src/main/java/com/cleverpush/inbox/InboxDetailBannerCarouselAdapter.java
@@ -23,6 +23,7 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -255,17 +256,22 @@ public class InboxDetailBannerCarouselAdapter extends RecyclerView.Adapter<Inbox
   }
 
   private void composeImageBlock(LinearLayout body, BannerImageBlock block, int position) {
-    @SuppressLint("InflateParams") FrameLayout imageLayout =
-            (FrameLayout) activity.getLayoutInflater().inflate(R.layout.app_banner_image, null);
-    AspectRatioImageView img = imageLayout.findViewById(R.id.imageView);
+    @SuppressLint("InflateParams") ConstraintLayout imageLayout =
+            (ConstraintLayout) activity.getLayoutInflater().inflate(R.layout.app_banner_image, null);
+    ImageView img = imageLayout.findViewById(R.id.imageView);
     ProgressBar progressBar = imageLayout.findViewById(R.id.progressBar);
     progressBar.setVisibility(View.VISIBLE);
-    int height = block.getImageHeight();
-    int width = block.getImageWidth();
 
-    float aspectRatio = (float) height / width;
+    ConstraintSet imgConstraints = new ConstraintSet();
+    imgConstraints.clone(imageLayout);
 
-    img.setAspectRatio(aspectRatio);
+    float widthPercentage = Math.min(100, Math.max(0, block.getScale())) / 100.0f;
+    imgConstraints.constrainPercentWidth(img.getId(), widthPercentage);
+    float aspectRatio = 1.0f;
+    float height = widthPercentage / aspectRatio * 100;
+    imgConstraints.constrainPercentHeight(img.getId(), height);
+
+    imgConstraints.applyTo(imageLayout);
 
     new Thread(() -> {
       try {
