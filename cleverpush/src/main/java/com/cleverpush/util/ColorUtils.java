1cf91bd0f5816047f946f9c32ab292135d70ae78
UnnatiCP <u.patadia@cleverpush.com>
2024-10-07 16:05:30 +0530

    hotfix/CP-8622-app-banners-check-app-version-filter-on-i-os-android
    1. Between is not working in App Version filter
    2. remove space at the end of the background colour.

--- cleverpush/src/main/java/com/cleverpush/util/ColorUtils.java
+++ cleverpush/src/main/java/com/cleverpush/util/ColorUtils.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 
 public class ColorUtils {
   public static int parseColor(String colorStr) {
+    if (colorStr != null) {
+      colorStr = colorStr.trim();
+    }
     if (colorStr != null && colorStr.length() == 4 && colorStr.charAt(0) == '#') {
       colorStr =
           "#" + colorStr.charAt(1) + colorStr.charAt(1) + colorStr.charAt(2) + colorStr.charAt(2) + colorStr.charAt(3)
