a606aa5f24018d72fb11323580872002c1905967
UnnatiCP <u.patadia@cleverpush.com>
2025-12-22 18:41:43 +0530

    Release 1.35.21
    Release 1.35.21

--- cleverpush/src/main/java/com/cleverpush/CleverPush.java
+++ cleverpush/src/main/java/com/cleverpush/CleverPush.java
@@ -135,7 +135,7 @@ import java.util.TimerTask;
 
 public class CleverPush {
 
-  public static final String SDK_VERSION = "1.35.20";
+  public static final String SDK_VERSION = "1.35.21";
 
   private static CleverPush instance;
   private static boolean isSubscribeForTopicsDialog = false;
