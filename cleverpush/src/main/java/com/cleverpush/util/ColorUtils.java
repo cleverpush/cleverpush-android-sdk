d12bc4335e615eba98dd572149d15a8d0f832e48
UnnatiCP <129381063+unnaticleverpush@users.noreply.github.com>
2026-03-12 16:12:10 +0530

    Fix button background color: support #RRGGBBAA (CSS) and avoid breaking #AARRGGBB (Android)
    Fixed button background color, support 8-digit hex colors.

--- cleverpush/src/main/java/com/cleverpush/util/ColorUtils.java
+++ cleverpush/src/main/java/com/cleverpush/util/ColorUtils.java
@@ -14,6 +14,17 @@ public class ColorUtils {
           "#" + colorStr.charAt(1) + colorStr.charAt(1) + colorStr.charAt(2) + colorStr.charAt(2) + colorStr.charAt(3)
               + colorStr.charAt(3);
     }
+    // Android expects #AARRGGBB (alpha first). If we have #RRGGBBAA (alpha last, e.g. from CSS/web),
+    // convert it. Only convert when Android would see alpha=00 (first 2 digits), so we don't break
+    // Android-style #AARRGGBB with non-zero alpha.
+    if (colorStr != null && colorStr.length() == 9 && colorStr.charAt(0) == '#'
+        && colorStr.substring(1, 3).equals("00")) {
+      String rr = colorStr.substring(1, 3);
+      String gg = colorStr.substring(3, 5);
+      String bb = colorStr.substring(5, 7);
+      String aa = colorStr.substring(7, 9);
+      colorStr = "#" + aa + rr + gg + bb;
+    }
     int color = Color.BLACK;
     try {
       color = Color.parseColor(colorStr);
